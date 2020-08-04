640145cd0a4d562c15a0bac199a00bec6f67dd41
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2020-08-04 12:16:26 +0700

    Menambahkan SW pada folder 404-not-found-master

---
+++ 404-not-found-master/app.js
@@ -0,0 +1,12 @@
+if('serviceWorker' in navigator) { // Apakah serviceWorker di support?
+  // jika true, lakukan registrasi
+  navigator.serviceWorker.register('./sw.js')
+   .then(reg => {
+    //  registrasi bekerja
+    console.log("Registrasi Sukses dengan cakupan: ", reg.scope);
+   }).catch(err => {
+    //  registrasi gagal
+    console.log("Registrasi Gagal:", err);
+   });
+}
+
